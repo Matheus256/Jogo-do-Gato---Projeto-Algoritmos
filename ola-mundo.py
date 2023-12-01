0972002283a7cf4354999347f46a08f31c6ab3bd
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2023-12-01 16:31:39 -0300

    Update ola-mundo.py

--- ola-mundo.py
+++ ola-mundo.py
@@ -1,20 +1,27 @@
-import tkinter
-import customtkinter  # <- import the CustomTkinter module
+import random
 
-root_tk = tkinter.Tk()  # create the Tk window like you normally do
-root_tk.geometry("400x240")
-root_tk.title("CustomTkinter Test")
 
-def button_function():
-    print("button pressed")
-
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=root_tk, corner_radius=10, command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-
-customtkinter.set_appearance_mode("Dark") # Other: "Light", "System" (only macOS)
-
-button = customtkinter.CTkButton(master=root_tk, fg_color=("black", "lightgray"), text="CTkButton",command=None)
-button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-
-root_tk.mainloop()
+casa=int(input("Digite a quantidade de casas: "))
+gato=random.randint(1,casa)
+casas=dict.fromkeys(list(range(1,casa+1)),'')
+casas[gato]='gato'
+while True:
+    tentativa=int(input())
+    while (
+    if (casas[tentativa]=='gato'):
+        print('achou')
+        break
+    else:
+        print(casas)
+        if (gato==casa):
+            casas[gato]=''
+            casas[casa-1]='gato'
+            gato=casa-1
+        elif (gato==1):
+            casas[gato]=''
+            casas[2]='gato'
+            gato=2
+        else:
+            casas[gato]=''
+            gato+=random.choice([-1,1])
+            casas[gato]='gato'
